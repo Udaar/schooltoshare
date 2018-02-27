129066d266a6f1d0f9930324a019f2d02eaac5ef
Mohamed <mohamed@localhost.localdomain>
2018-02-27 16:07:27 +0200

    redirect to login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,10 @@ class HomeController extends Controller
    
     public function index()
     {
-        return view('bimunity.index');
+        if(\Auth::check())
+            return redirect('home');
+        else
+            return redirect('login');
     }
     public function dashboard()
     {
