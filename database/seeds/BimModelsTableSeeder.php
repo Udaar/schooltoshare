2cacc7286708747f0eab2cf544817d6bdff9afc6
Mohamed <mohamed@localhost.localdomain>
2018-02-28 14:30:39 +0200

    school model

--- database/seeds/BimModelsTableSeeder.php
+++ database/seeds/BimModelsTableSeeder.php
@@ -13,124 +13,86 @@ class BimModelsTableSeeder extends Seeder
     {
         DB::table('bim_models')->delete();
          App\Models\BimModel::create([
-            'name' => 'tower',
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
             'discipline_code'=> 'ARC',
-            'description' => 'Tower Model',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTA1LTIwLTI4LTM1LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUl9UUkxfVFdSX0FSQy0wMS5ydnQ',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
-         App\Models\BimModel::create([
-            'name' => 'unit',
+        App\Models\BimModel::create([
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'STR',
-            'description' => 'Tower Model',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTEwLTE2LTI2LTE4LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUl9UUkxfVU5UX0FSQy1WMDFBLnJ2dA',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
-         App\Models\BimModel::create([
-            'name' => 'faisal',
+        App\Models\BimModel::create([
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'ELE',
-            'description' => 'Faisal Bank',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTIzLTA0LTE1LTIyLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlLzE3RUdDUjAwMS1GSUItWFhYLU1EMy1BUkMtWFgtQUwtQVotMDAwMDEtVjAxQS5ydnQ',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
         App\Models\BimModel::create([
-            'name' => 'mall',
-            'building_id'=> 2,
-            'project_code'=>'xxx',
-            'organization_code'=> 'ddd',
-            'document_type_code'=>'sss',
-            'discipline_code'=> 'MHV',
-            'description' => 'mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTM0LTM5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1JldGFpbCUyMFNhbXBsZS5ydnQ',
-        ]);
-        App\Models\BimModel::create([
-            'name' => 'ADmall',
-            'building_id'=> 2,
-            'project_code'=>'xxx',
-            'organization_code'=> 'ddd',
-            'document_type_code'=>'sss',
-            'discipline_code'=> 'FRS',
-            'description' => 'abu dhabi mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTEzLTQwLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL0FETS0wMDEucnZ0',
-        ]);
-        App\Models\BimModel::create([
-            'name' => 'tower',
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'PDS',
-            'description' => 'Tower Model',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTA1LTIwLTI4LTM1LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUl9UUkxfVFdSX0FSQy0wMS5ydnQ',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
-         App\Models\BimModel::create([
-            'name' => 'unit',
+        App\Models\BimModel::create([
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'INF',
-            'description' => 'Tower Model',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTEwLTE2LTI2LTE4LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUl9UUkxfVU5UX0FSQy1WMDFBLnJ2dA',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
-         App\Models\BimModel::create([
-            'name' => 'faisal',
+        App\Models\BimModel::create([
+            'name' => 'School',
             'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'LSC',
-            'description' => 'Faisal Bank',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA0LTIzLTA0LTE1LTIyLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlLzE3RUdDUjAwMS1GSUItWFhYLU1EMy1BUkMtWFgtQUwtQVotMDAwMDEtVjAxQS5ydnQ',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
         App\Models\BimModel::create([
-            'name' => 'mall',
-            'building_id'=> 2,
+            'name' => 'School',
+            'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'CIV',
-            'description' => 'mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTM0LTM5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1JldGFpbCUyMFNhbXBsZS5ydnQ',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
+
         App\Models\BimModel::create([
-            'name' => 'ADmall',
-            'building_id'=> 2,
+            'name' => 'School',
+            'building_id'=> 1,
             'project_code'=>'xxx',
             'organization_code'=> 'ddd',
             'document_type_code'=>'sss',
-            'discipline_code'=> 'TEL',
-            'description' => 'abu dhabi mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTEzLTQwLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL0FETS0wMDEucnZ0',
+            'discipline_code'=> 'ARC',
+            'description' => 'School Model',
+            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI0LTA2LTA1LTE5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1VEUi1UU0MtQVJDLVYwMUQucnZ0',
         ]);
-        App\Models\BimModel::create([
-            'name' => 'mall',
-            'building_id'=> 2,
-            'project_code'=>'xxx',
-            'organization_code'=> 'ddd',
-            'document_type_code'=>'sss',
-            'discipline_code'=> 'FLS',
-            'description' => 'mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTM0LTM5LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL1JldGFpbCUyMFNhbXBsZS5ydnQ',
-        ]);
-        App\Models\BimModel::create([
-            'name' => 'ADmall',
-            'building_id'=> 2,
-            'project_code'=>'xxx',
-            'organization_code'=> 'ddd',
-            'document_type_code'=>'sss',
-            'discipline_code'=> 'MLD',
-            'description' => 'abu dhabi mall',
-            'urn' => 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTA3LTE0LTEzLTEzLTQwLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL0FETS0wMDEucnZ0',
-        ]);
+         
     }
 }
